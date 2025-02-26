fc85d7c85911a33b25477ca1a4cf4c0a256e2e5d
Andriantsitohaina Hajaina <74816899+mamyhajaina@users.noreply.github.com>
2025-02-27 01:42:30 +0300

    sdcs

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -105,20 +105,6 @@ const Footer = () => {
             ))}
         </ul>
     </div>
-
-    <div
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
-        text-center pt-2 text-gray-400 text-sm pb-8"
-    >
-        <span>© 2023 Om Pattjoshi. Tous droits réservés.</span>
-        <span>Conditions · Politique de confidentialité</span>
-        <div className="sm:block flex items-center justify-center w-full">
-            <img
-                src="https://hamart-shop.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffooter-payment.a37c49ac.png&w=640&q=75"
-                alt="Méthodes de paiement"
-            />
-        </div>
-    </div>
 </div>
 
     );
