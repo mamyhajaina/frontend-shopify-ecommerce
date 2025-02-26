e05305c43373e336cf40fe536161cf37fd21a2a5
Andriantsitohaina Hajaina <74816899+mamyhajaina@users.noreply.github.com>
2025-02-27 01:50:33 +0300

    kjdfv

--- src/components/Route/Hero/Hero.jsx
+++ src/components/Route/Hero/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
         <Link to="/products" className="inline-block">
             <div className={`${styles.button} mt-5`}>
                 <span className="text-[#fff] font-[Poppins] text-[18px]">
-                    Découvrez nos produits
+                    Nos produits
                 </span>
             </div>
         </Link>
